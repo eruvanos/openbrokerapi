dbea1347670a2d11b28aa9f87f1a59f5e381668b
Eruvanos <eruvanos@ewetel.net>
2017-06-02 15:02:31 +0200

    automatic push to pypitest and tagged to pypi through travis (bumping patch version per commit)

--- ship.sh
+++ ship.sh
@@ -1,7 +1,30 @@
-set -e
+#! /bin/bash
+set -ex
 
+args=""
+if [[ $# == 0 ]]; then
+    args="patch"
+else
+    args=$*
+fi
+
+echo "Pull changes"
 git pull -r
 
+echo "Run tests"
 ./setup.py test
 
+echo "Bump version"
+#Bump version
+bumpversion $args
+
+echo "Add version changes to commit"
+git add .bumpversion.cfg
+git add setup.py
+git commit --amend --no-edit
+
+#seperate commit with version in comment
+#git commit -m "v$(cat .bumpversion.cfg|grep current_version|tr -d ' '|cut -f 2 -d '=')""
+
+echo "Push to git"
 git push
