a932a0c724f536066cd5219b4ecd07a5826cc223
Eruvanos <eruvanos@ewetel.net>
2017-06-02 15:22:36 +0200

    install pypandoc to fix missing long_description in deployment

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_long_description():
 
 setup(
     name='openbrokerapi',
-    version='0.3.3',
+    version='0.3.4',
     packages=find_packages(),
     install_requires=['flask'],
     test_suite="test",
