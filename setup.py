e853a052df2c6455cdfa6f6ae6f8c4f1eefbf7dc
Eruvanos <eruvanos@ewetel.net>
2017-12-06 10:35:12 +0100

    Bump version: 0.5.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='openbrokerapi',
-    version='0.5.0',
+    version='1.0.0',
     python_requires='>=3.5',
     packages=find_packages(),
     install_requires=['flask'],
