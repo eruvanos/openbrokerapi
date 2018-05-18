8024b858f85358f57071fc022a1f6a63e13e1186
Eruvanos <maic@siemering.tech>
2018-05-18 11:42:05 +0200

    Fix setup.py test execution

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='openbrokerapi',
-    version='3.0.3',
+    version='3.0.4',
     python_requires='>=3.5',
     packages=find_packages(exclude=['tests']),
     install_requires=['flask', 'gevent'],
-    test_suite="test",
+    test_suite="tests",
 
     # Metadata
     author="Maic Siemering",
