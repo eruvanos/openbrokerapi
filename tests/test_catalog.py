246b1a34f8f4bd5b19c953c7098b634918c13486
Eruvanos <maic@siemering.tech>
2019-01-15 21:21:20 +0100

    Cleanup

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -1,31 +1,30 @@
-import http
 import base64
+import http
 
 from openbrokerapi.catalog import (
     ServiceDashboardClient,
     ServiceMetadata,
     ServicePlan,
     ServicePlanCost,
-    ServicePlanMetaData,
-)
+    ServicePlanMetadata)
 from openbrokerapi.service_broker import Service
 from tests import BrokerTestCase
 
 
 class CatalogTest(BrokerTestCase):
     service = Service(
-        id="s1",
-        name="service_name",
-        description="service_description",
+        id='s1',
+        name='service_name',
+        description='service_description',
         bindable=True,
-        plans=[ServicePlan(id="p1", name="default", description="plan_description")]
+        plans=[ServicePlan(id='p1', name='default', description='plan_description')]
     )
 
     def test_catalog_called_with_the_right_values(self):
         self.broker.catalog.return_value = self.service
 
         self.client.get(
-            "/v2/catalog",
+            '/v2/catalog',
             headers={
                 'X-Broker-Api-Version': '2.13',
                 'Authorization': self.auth_header
@@ -37,31 +36,34 @@ class CatalogTest(BrokerTestCase):
         self.broker.catalog.return_value = self.service
 
         self.client.get(
-            "/v2/catalog",
+            '/v2/catalog',
             headers={
                 'X-Broker-Api-Version': '2.13',
                 'Authorization': self.auth_header,
-                "unknown": "unknown"
+                'unknown': 'unknown'
             })
 
         self.assertTrue(self.broker.catalog.called)
 
     def test_catalog_returns_200_with_service_information(self):
+        '''
+        This tests all the possible information a catalog could have. Including custom Service-/ServicePlanMetadata
+        '''
         self.broker.catalog.return_value = Service(
-            id="s1",
-            name="service_name",
-            description="service_description",
+            id='s1',
+            name='service_name',
+            description='service_description',
             bindable=True,
             plans=[ServicePlan(
-                id="p1",
-                name="default",
-                description="plan_description",
-                metadata=ServicePlanMetaData(
-                    displayName="displayName",
-                    bullets=["bullet1"],
+                id='p1',
+                name='default',
+                description='plan_description',
+                metadata=ServicePlanMetadata(
+                    displayName='displayName',
+                    bullets=['bullet1'],
                     costs=[ServicePlanCost(
-                        amount={"requests": 1},
-                        unit="unit"
+                        amount={'requests': 1},
+                        unit='unit'
                     )]
                 ),
                 free=True,
@@ -70,48 +72,48 @@ class CatalogTest(BrokerTestCase):
             tags=['tag1', 'tag2'],
             requires=['something'],
             metadata=ServiceMetadata(
-                displayName="displayName",
-                imageUrl="imageUrl",
-                longDescription="longDescription",
-                providerDisplayName="providerDisplayName",
-                documentationUrl="documentationUrl",
-                supportUrl="supportUrl",
+                displayName='displayName',
+                imageUrl='imageUrl',
+                longDescription='longDescription',
+                providerDisplayName='providerDisplayName',
+                documentationUrl='documentationUrl',
+                supportUrl='supportUrl',
                 shareable=True
             ),
             dashboard_client=ServiceDashboardClient(
-                id="id",
-                secret="secret",
-                redirect_uri="redirect_uri"
+                id='id',
+                secret='secret',
+                redirect_uri='redirect_uri'
             ),
             plan_updateable=True
         )
 
         response = self.client.get(
-            "/v2/catalog",
+            '/v2/catalog',
             headers={
                 'X-Broker-Api-Version': '2.13',
                 'Authorization': self.auth_header,
-                "unknown": "unknown"
+                'unknown': 'unknown'
             })
 
         self.assertEqual(response.status_code, http.HTTPStatus.OK)
         self.assertEqual(response.json,
                          dict(
                              services=[
-                                 dict(id="s1",
-                                      name="service_name",
-                                      description="service_description",
+                                 dict(id='s1',
+                                      name='service_name',
+                                      description='service_description',
                                       bindable=True,
                                       plans=[dict(
-                                          id="p1",
-                                          name="default",
-                                          description="plan_description",
+                                          id='p1',
+                                          name='default',
+                                          description='plan_description',
                                           metadata=dict(
-                                              displayName="displayName",
-                                              bullets=["bullet1"],
+                                              displayName='displayName',
+                                              bullets=['bullet1'],
                                               costs=[dict(
-                                                  amount={"requests": 1},
-                                                  unit="unit"
+                                                  amount={'requests': 1},
+                                                  unit='unit'
                                               )]
                                           ),
                                           free=True,
@@ -120,18 +122,19 @@ class CatalogTest(BrokerTestCase):
                                       tags=['tag1', 'tag2'],
                                       requires=['something'],
                                       metadata=dict(
-                                          displayName="displayName",
-                                          imageUrl="imageUrl",
-                                          longDescription="longDescription",
-                                          providerDisplayName="providerDisplayName",
-                                          documentationUrl="documentationUrl",
-                                          supportUrl="supportUrl",
-                                          shareable=True
+                                          displayName='displayName',
+                                          imageUrl='imageUrl',
+                                          longDescription='longDescription',
+                                          providerDisplayName='providerDisplayName',
+                                          documentationUrl='documentationUrl',
+                                          supportUrl='supportUrl',
+                                          shareable=True,
+                                          custom_field1='custom_field1'
                                       ),
                                       dashboard_client=dict(
-                                          id="id",
-                                          secret="secret",
-                                          redirect_uri="redirect_uri"
+                                          id='id',
+                                          secret='secret',
+                                          redirect_uri='redirect_uri'
                                       ),
                                       plan_updateable=True
                                       )
@@ -142,12 +145,13 @@ class CatalogTest(BrokerTestCase):
         self.broker.catalog.return_value = self.service
 
         response = self.client.get(
-            "/v2/catalog",
+            '/v2/catalog',
             headers={
                 'X-Broker-Api-Version': '2.13',
-                'X-Broker-Api-Originating-Identity': 'test '+base64.standard_b64encode(b'{"user_id":123}').decode('ascii'),
+                'X-Broker-Api-Originating-Identity': 'test ' + base64.standard_b64encode(b'{"user_id":123}').decode(
+                    'ascii'),
                 'Authorization': self.auth_header,
-                "unknown": "unknown"
+                'unknown': 'unknown'
             })
 
         self.assertEqual(response.status_code, http.HTTPStatus.OK)
@@ -155,29 +159,29 @@ class CatalogTest(BrokerTestCase):
                          dict(
                              services=[
                                  dict(
-                                     id="s1",
-                                     name="service_name",
-                                     description="service_description",
+                                     id='s1',
+                                     name='service_name',
+                                     description='service_description',
                                      bindable=True,
                                      plan_updateable=False,
-                                     plans=[dict(id="p1", name="default", description="plan_description")]
+                                     plans=[dict(id='p1', name='default', description='plan_description')]
                                  )
                              ]
                          ))
 
     def test_catalog_returns_500_if_error_raised(self):
-        self.broker.catalog.side_effect = Exception("ERROR")
+        self.broker.catalog.side_effect = Exception('ERROR')
 
         response = self.client.get(
-            "/v2/catalog",
+            '/v2/catalog',
             headers={
                 'X-Broker-Api-Version': '2.13',
                 'Authorization': self.auth_header,
-                "unknown": "unknown"
+                'unknown': 'unknown'
             })
 
         self.assertEqual(response.status_code, http.HTTPStatus.INTERNAL_SERVER_ERROR)
         self.assertEqual(response.json,
                          dict(
-                             description="ERROR"
+                             description='ERROR'
                          ))
