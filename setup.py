e7b1200ca91b9f8bffc7299308d3c3f896dd9bb3
Eruvanos <maic@siemering.tech>
2019-01-15 21:40:52 +0100

    Bump version: 3.2.4 → 3.2.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='openbrokerapi',
-    version='3.2.4',
+    version='3.2.5',
     python_requires='>=3.5',
     packages=find_packages(exclude=['tests']),
     install_requires=['flask', 'gevent'],
