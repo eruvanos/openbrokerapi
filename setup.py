4441a8a6f861c8aa8dc9ec430810f6adef1596d2
Maic Siemering <maic@siemering.tech>
2020-02-11 14:25:29 +0100

    Bump version: 4.0.1 → 4.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='openbrokerapi',
-    version='4.0.1',
+    version='4.0.2',
     python_requires='>=3.5',
     packages=find_packages(exclude=['tests']),
     install_requires=['flask'],
