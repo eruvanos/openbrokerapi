32b5e13998fb0bccc80e04d73303fa91722deba8
Maic Siemering <maic@vw-dilab.com>
2021-03-12 23:03:12 +0100

    Bump version: 4.1.1 → 4.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='openbrokerapi',
-    version='4.1.1',
+    version='4.1.2',
     python_requires='>=3.6',
     packages=find_packages(exclude=['tests']),
     install_requires=['flask'],
